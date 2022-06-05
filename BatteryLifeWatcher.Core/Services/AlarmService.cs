91b11c6fef354d0c1fe529be002f0a078af5fdf5
hdhilip98 <dhiliph98@outlook.com>
2022-06-05 22:02:59 +0530

    Add default sound file and verify if file exists

--- BatteryLifeWatcher.Core/Services/AlarmService.cs
+++ BatteryLifeWatcher.Core/Services/AlarmService.cs
@@ -1,4 +1,6 @@
 ﻿using BatteryLifeWatcher.Core.States.Alarm;
+using System;
+using System.IO;
 using System.Media;
 
 namespace BatteryLifeWatcher.Core.Services {
@@ -7,6 +9,15 @@ namespace BatteryLifeWatcher.Core.Services {
         private IAlarmState _currentState;
 
         public AlarmService(string soundPath) {
+            if (string.IsNullOrEmpty(soundPath))
+                throw new ArgumentNullException();
+
+            if (!File.Exists(soundPath))
+                throw new ArgumentException("Sound file does not exist.");
+
+            if (Path.GetExtension(soundPath) != ".wav")
+                throw new ArgumentException("Invalid sound format. Only .wav files are supported");
+
             _player = new SoundPlayer(soundPath);
             _currentState = new Off(_player);
         }
