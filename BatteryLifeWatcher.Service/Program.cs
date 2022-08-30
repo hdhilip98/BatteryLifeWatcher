cf6ed3609215763ff91e3cf81dd616e3af5807f5
hdhilip98 <dhiliph98@outlook.com>
2022-08-30 23:17:05 +0530

    Customize installer to get min and max values from user during installation

--- BatteryLifeWatcher.Service/Program.cs
+++ BatteryLifeWatcher.Service/Program.cs
@@ -1,13 +1,26 @@
-﻿using System;
+﻿using BatteryLifeWatcher.Core;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Reflection;
 using Topshelf;
 
 namespace BatteryLifeWatcher.Service {
     internal class Program {
+        static float _min = 0.2f;
+        static float _max = 0.8f;
+        static string _serviceDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        static string _configPath = Path.Combine(_serviceDir, "WatcherConfig.json");
+        static string _soundPath = Path.Combine(_serviceDir, @"Sounds\default.wav");
+
         static void Main(string[] args) {
             var rc = HostFactory.Run(x => {
                 x.Service<Service>(s => {
                     s.ConstructUsing(_ => new Service());
-                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStarted(tc => {
+                        var config = LoadConfiguration();
+                        tc.Start(config);
+                    });
                     s.WhenStopped(tc => tc.Stop());
                 });
 
@@ -19,14 +32,45 @@ namespace BatteryLifeWatcher.Service {
                     r.OnCrashOnly();
                     r.SetResetPeriod(1);
                 });
-
+                
                 x.SetDescription("Alerts the user when battery level falls above or below the user defined level.");
                 x.SetDisplayName("Battery Life Watcher");
                 x.SetServiceName("BatteryLifeWatcher");
+
+                x.AddCommandLineDefinition("min", v => _min = float.Parse(v) / 100);
+                x.AddCommandLineDefinition("max", v => _max = float.Parse(v) / 100);
+
+                x.AfterInstall(() => {
+                    DeleteConfiguration();
+                    CreateDefaultConfiguration();
+                });
+
+                x.AfterUninstall(() => {
+                    DeleteConfiguration();
+                });
             });
 
             var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());
             Environment.ExitCode = exitCode;
+        }
+
+        static void DeleteConfiguration() {
+            if (File.Exists(_configPath)) File.Delete(_configPath);
+        }
+
+        static WatcherConfig LoadConfiguration() {
+            if(!File.Exists(_configPath)) {
+                return CreateDefaultConfiguration();
+            }
+
+            var json = File.ReadAllText(_configPath);
+            return JsonConvert.DeserializeObject<WatcherConfig>(json);
+        }
+
+        static WatcherConfig CreateDefaultConfiguration() {
+            var config = new WatcherConfig(_min, _max, _soundPath);
+            File.WriteAllText(_configPath, JsonConvert.SerializeObject(config));
+            return config;
         }
     }
 }
--- BatteryLifeWatcher.Service/Service.cs
+++ BatteryLifeWatcher.Service/Service.cs
@@ -1,13 +1,9 @@
 ﻿using BatteryLifeWatcher.Core;
-using Newtonsoft.Json;
-using System.IO;
-using System.Reflection;
 
 namespace BatteryLifeWatcher.Service {
     internal class Service {
         private Watcher _watcher;
-        public void Start() {
-            var config = LoadConfiguration();
+        public void Start(WatcherConfig config) {
             _watcher = new Watcher(config);
             _watcher.Start();
         }
@@ -15,13 +11,6 @@ namespace BatteryLifeWatcher.Service {
         public void Stop() {
             _watcher.Stop();
             _watcher = null;
-        }
-
-        private WatcherConfig LoadConfiguration() {
-            var serviceDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            var configPath = Path.Combine(serviceDir, "WatcherConfig.json");
-            var json = File.ReadAllText(configPath);
-            return JsonConvert.DeserializeObject<WatcherConfig>(json);
         }
     }
 }
